1856fe85bbc9f2a38acceb4340277f7c56409b87
rokari-19 <oshomahiyelobu1@gmail.com>
2026-05-13 15:56:18 +0100

    added dynamic schema typing to insert() in BaseAdapter

--- migration_engine/domain/adapters/base.py
+++ migration_engine/domain/adapters/base.py
@@ -13,8 +13,20 @@ class BaseAdapter:
         columns = list(data[0].keys())
         cols_str = ", ".join(columns)
         placeholders = ", ".join([self.placeholder] * len(columns))
-        sql = f"INSERT INTO {table_name} ({cols_str}) VALUES ({placeholders})"
         cursor = self.conn.cursor()
+        sql = f"INSERT INTO {table_name} ({cols_str}) VALUES ({placeholders})"
+
+        create_cols = ",\n".join(
+            [f"{col} VARCHAR(255) NOT NULL" for col in columns]
+        )
+
+        cursor.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {table_name} (
+                {create_cols}
+            );
+            """
+        )
         for row in data:
             cursor.execute(sql, tuple(row.get(c) for c in columns))
         self.conn.commit()
