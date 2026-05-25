0a02ef1925b652026aea8eb3283f1e6f2fcef3ca
Ajiri Oshomah Iyelobu <oshomahiyelobu1@gmail.com>
2026-05-25 10:35:31 +0100

    Configure production-ready Django staticfiles settings

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
